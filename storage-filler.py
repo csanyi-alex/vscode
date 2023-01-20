fe81a55cf6bc254a41d552763724326e1acc7fbf
Your Name <you@example.com>
2023-01-20 11:26:29 +0100

    yes

--- storage-filler.py
+++ storage-filler.py
@@ -1,10 +1,11 @@
 try:
-    number = int(input("How big do u want it to be? (might crash potato PCs)"))
+    size = int(input("How big do u want it to be? (might crash potato PCs)"))
 
 except:
     print ("Value error ._.")
 
 filler = open("text.txt", "a")
-for i in range(0,12):
+
+for i in range(0,size):
     filler.write(str(123456**i))
     filler.flush()
